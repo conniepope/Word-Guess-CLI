77cc87b5ed298748af5f9d4b8d1178b2b5eda0fb
conniepope <conniepope77@gmail.com>
2019-07-04 18:23:43 -0400

    initial files, dependency set up

---
+++ index.js
@@ -0,0 +1,10 @@
+var Word = require("./word.js")
+
+// The logic for the course of the game
+Word = randomWord;
+var randomWord = //some kind of math
+var options = ["alphabet", "jitterbug", "bologna", "conducted", "dinosaur", "egotistical", "forensics", "imagination", "kilowatt"]
+
+//   * Randomly selects a word and uses the `Word` constructor to store it
+
+//   * Prompts the user for each guess and keeps track of the user's remaining guesses
---
+++ letter.js
@@ -0,0 +1,21 @@
+// Dependency for inquirer npm package
+var inquirer = require("inquirer");
+
+// Is this line needed?
+var fs = require("fs")
+
+function Letter() {
+    stringValue: "letter";
+    letterGuessed: true;
+    
+}
+
+// Make a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
+
+//   * A string value to store the underlying character for the letter
+
+//   * A boolean value that stores whether that letter has been guessed yet
+
+//   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+
+//   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
